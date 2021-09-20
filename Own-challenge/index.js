f2825211015b79c028871b3efcc4634693abaf32
hamasurrab <hamasurrab2@gmail.com>
2021-09-20 21:19:08 +0530

    i got bug in my challenge

--- Own-challenge/index.js
+++ Own-challenge/index.js
@@ -1,4 +1,11 @@
 var whoIs=Math.random()*2;
 whoIs=Math.floor(whoIs)+1;
+
 var randomImage="image/"+whoIs+".jpg";
 var image=document.querySelectorAll("img")[0].setAttribute("src",randomImage);
+if(whoIs=1){
+    document.querySelector("h1").innerHTML="chaudhary saifurrab";
+}
+else {
+    document.querySelector("h1").innerHTML="chaudhary Hamasurrab";
+}
