9e1508547ca8fd20db6195dbabb1a906445ba3ac
hamasurrab <hamasurrab2@gmail.com>
2021-09-20 21:23:42 +0530

    solve problem(innerHtml text was not working)

--- Own-challenge/index.js
+++ Own-challenge/index.js
@@ -3,9 +3,9 @@ whoIs=Math.floor(whoIs)+1;
 
 var randomImage="image/"+whoIs+".jpg";
 var image=document.querySelectorAll("img")[0].setAttribute("src",randomImage);
-if(whoIs=1){
-    document.querySelector("h1").innerHTML="chaudhary saifurrab";
+if(whoIs<2){
+    document.querySelector("h1").innerHTML="Chaudhary saifurrab";
 }
 else {
-    document.querySelector("h1").innerHTML="chaudhary Hamasurrab";
+    document.querySelector("h1").innerHTML="Chaudhary Hamasurrab";
 }
