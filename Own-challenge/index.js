75568d34c139a4f407bdb3e0968df08bb754f316
hamasurrab <hamasurrab2@gmail.com>
2021-09-20 21:06:32 +0530

    successful in creating in own challenge

--- Own-challenge/index.js
+++ Own-challenge/index.js
@@ -1,1 +1,4 @@
-alert("hamas");
+var whoIs=Math.random()*2;
+whoIs=Math.floor(whoIs)+1;
+var randomImage="image/"+whoIs+".jpg";
+var image=document.querySelectorAll("img")[0].setAttribute("src",randomImage);
