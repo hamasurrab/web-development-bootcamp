fe1fe776331e97d264c61d0b2f77d26aa9e46a71
hamasurrab <hamasurrab2@gmail.com>
2021-09-21 14:41:11 +0530

    deletion own

---
+++ Challenge-2/index.js
@@ -0,0 +1,17 @@
+var whoIs=Math.random()*3;
+whoIs=Math.floor(whoIs)+1;
+
+var randomImage="image/"+whoIs+".jpg";
+var image=document.querySelectorAll("img")[0].setAttribute("src",randomImage);
+if(whoIs<2){
+    document.querySelector("h1").innerHTML="Shahrukh khan";
+    document.querySelector("p").innerHTML="Actor";
+}
+else if(whoIs==2) {
+    document.querySelector("h1").innerHTML="Cristiano ronaldo";
+    document.querySelector("p").innerHTML="footballer";
+}
+else{
+    document.querySelector("h1").innerHTML="Anushka sharma";
+    document.querySelector("p").innerHTML="Indian Actress";
+}
