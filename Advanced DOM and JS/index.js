528dd1f53c9d0fddaaa2c6963c9c20a6c139e928
hamasurrab <hamasurrab2@gmail.com>
2021-10-07 16:28:49 +0530

    slice keyword

--- Advanced DOM and JS/index.js
+++ Advanced DOM and JS/index.js
@@ -1,3 +1,1 @@
-var tweet=prompt("compose your tweet");
-var tweetCount=tweet.length;
-alert("you have written "+tweetCount+"character "+"your tweet character left "+140-tweetCount+".");
+alert(prompt("compose ur tweet").slice(0,10));
