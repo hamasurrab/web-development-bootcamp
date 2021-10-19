0e25004e7b9a93d2e427be44bfd30252fdd22cc4
hamasurrab <hamasurrab2@gmail.com>
2021-10-19 17:07:57 +0530

    intro to node.js and learn how to install npm and apply it

--- Backend web development/Node.js/index.js
+++ Backend web development/Node.js/index.js
@@ -1,3 +1,23 @@
 // checking output in hyperlink
-console.log("hello world!");
+// console.log("hello world!");
 // alert("hey i am hamas chaudhary");
+
+
+
+// //jshint esversion:6
+// const fs = require("fs");
+// fs.copyFileSync("file1.txt","file2.txt");
+
+
+
+var superheroes = require("superheroes");
+
+var supervillains = require("supervillains");
+
+var mySuperheroName=superheroes.random();
+
+var mySupervillainName=supervillains.random();
+
+console.log(mySuperheroName);
+
+console.log(mySupervillainName);
