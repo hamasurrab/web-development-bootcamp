7418b9b2a45aa682f5a39de8314db0465c4f301a
hamasurrab <hamasurrab2@gmail.com>
2021-10-30 16:45:23 +0530

    frontend

---
+++ Frontend web development/previous/index.js
@@ -0,0 +1,17 @@
+$("h1").text("hey i am chaudhary");
+$("h1").css("color","red");
+$("a").attr("href", "https://www.bing.com");
+$("h1").click(function(){
+   
+
+$("h1").click(function(){
+    
+$("h1").css("color","yellow");
+});
+ $("h1").css("color","green");
+
+});
+
+$(document).on("keypress", function(){
+    $("h1").text("hey");
+});
