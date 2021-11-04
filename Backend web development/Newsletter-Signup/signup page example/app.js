7ad12182c5e804488df7c8db9b85abec69f83442
hamasurrab <hamasurrab2@gmail.com>
2021-11-04 17:14:42 +0530

    deploying our server to mailchimp

--- Backend web development/Newsletter-Signup/signup page example/app.js
+++ Backend web development/Newsletter-Signup/signup page example/app.js
@@ -1,5 +1,6 @@
 const express =require("express");
 const bodyParser=require("body-parser");
+const request=require("request");
 
 const app =express();
 app.use(express.static("public"));
@@ -22,14 +23,88 @@ app.get("/",function(req,res){
 
 app.post("/",function(req,res){
 
-var firstName=req.body.fName;
-var lastName=req.body.lName;
-var email=req.body.Email;
+// var firstName=req.body.fName;
+// var lastName=req.body.lName;
+// var email=req.body.Email;
 
-console.log(firstName,lastName,email);
-})
+// console.log(firstName,lastName,email);
+
+const {firstName,lastName,email} = req.body;
+
+//make sure fields are fielled
+if(!firstName|| !lastName|| !email){
+    res.redirect("failure.html");
+    return;
+}
+
+
+//construct req data
+const data={
+    members:[
+        {
+            email_address:email,
+            status:"subscribed",
+            merge_fields:{
+                FName:firstName,
+                LName:lastName
+            }
+        }
+    ]
+}
+
+
+const postData=JSON.stringify(data);
+
+
+
+
+
+
+const options={
+    url:"https://${us5}.api.mailchimp.com/lists/4d33a30303",
+    method:"POST",
+    Headers:{
+        authorization:"auth aef01a1c743877d9affb85d26db458a4-us5"
+    },
+    body:postData
+}
+
+
+
+request(options,function(err,response,body){
+    if(err){
+        res.redirect("/failure.html");
+    }else{
+        if(response.statusCode===200){
+           res.redirect("success.html"); 
+        }else{
+            res.redirect("/failure.html");
+        }
+    }
+
+});
+
+});
 
 
 app.listen(3000,function () {
     console.log("port is ruuning on port 3000");
 });
+
+
+
+
+
+//api key aef01a1c743877d9affb85d26db458a4-us5
+// audience id 4d33a30303
+
+
+
+
+
+// dc="YOUR_DC"
+// apikey="YOUR_API_KEY"
+
+// curl -sS \
+//   "https://${dc}.api.mailchimp.com/3.0/ping" \
+//   --user "anystring:${apikey}" | jq -r
